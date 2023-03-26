1f69f8083983fd17e11a61c291faac39a1744851
Saad Wasim <105033537+Verifiedz@users.noreply.github.com>
2023-03-26 00:42:14 -0400

    Create Silver.java

---
+++ Silver.java
@@ -0,0 +1,10 @@
+package Project;
+public class Silver extends State {
+
+    public Silver(){
+        getRank();
+    }
+    public void getRank(){
+        System.out.println("Rank:Silver");
+    }
+}
