83968d68728b7a462a0cca8a69d1f59a74faf907
Saad Wasim <105033537+Verifiedz@users.noreply.github.com>
2023-03-26 00:41:09 -0400

    Create Book.java

---
+++ Book.java
@@ -0,0 +1,20 @@
+package Project;
+
+/**
+ *
+ * @author feroz
+ */
+public class Book {
+    
+    String name;
+    int price;
+    
+    public String getBookName() {
+        return name;
+    }
+
+    public int getBookPrice() {
+        return price;
+    }
+    
+}
