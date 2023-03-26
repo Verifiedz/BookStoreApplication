9173a767754ab56a47d8207194ed5727b8267919
Saad Wasim <105033537+Verifiedz@users.noreply.github.com>
2023-03-26 00:39:26 -0400

    Create Customer.java

---
+++ Customer.java
@@ -0,0 +1,89 @@
+package Project;
+import java.util.Scanner;
+public class Customer extends User {
+    private int Cpoints; 
+    private State myState; 
+    private String intial = null; 
+    public Customer(){
+    }
+    public String getUsername(){
+        return username;
+    }
+    public String getPassword(){
+        return password;
+    }
+    public int getPoints(){
+        return Cpoints;
+    }
+    public void setPoints(int s ){
+        Cpoints =s;
+    }
+    public void SetUsername(String s){
+        username = s;
+    }
+    public void SetPassword(String s){
+        
+        if(intial == null){ 
+            password = s;
+            intial = "LockedPassword";
+        }
+        
+    }
+    public void ChangePassword(Customer s, String k, String p) throws IllegalAccessError{
+        Scanner scan = new Scanner(System.in);
+        int counter = 0;
+        if(s.getPassword() ==k){
+            intial = null;
+            s.SetPassword(p);
+        }
+        else{
+            while(counter<3){
+                    System.out.println("Im sorry incorrect password--> enter password again");
+                     k = scan.next();
+                     if(s.getPassword() ==k){
+                        intial = null;
+                        s.SetPassword(p);
+                        counter = 2;
+                    }
+                    counter++;
+            }
+            if(s.getPassword()!=p) throw new IllegalAccessError("Surpassed Allowed Attempts: Try again later");
+        }
+        System.out.println("Password was Successfully changed");
+
+    }
+    // need to fix this*
+    public void setStatus(){
+        if(Cpoints>=1000) myState = new Gold();
+        else myState = new Silver();
+    }
+    @Override
+    public String toString(){
+        String clist ="Username:"+getUsername()+"\n"+"Password:"+getPassword()+"\n"+"Points:"+getPoints();
+        System.out.println(clist);
+        System.out.println();
+        return clist;
+    }
+    //testing methods//
+    public static void main(String args[]){
+    Customer s = new Customer();
+    s.SetPassword("abc123");
+    s.SetPassword("myname");
+    s.ChangePassword(s, "abc123", "myname");
+    s.SetUsername("Melvin");
+     s.setPoints(1500);
+     s.setStatus();
+     s.toString();
+     Customer c = new Customer();
+     Customer l = new Customer();
+     l.SetUsername("Roger");
+     l.SetPassword("abc123");
+     l.SetUsername("Saad");
+     l.SetPassword("abc123");
+     l.setPoints(500);
+     s.setStatus();
+    l.toString();
+    
+    }
+    
+}
