d543e1b1dd3067ad61ad30c2f84e10d18bdcc26e
Saad Wasim <105033537+Verifiedz@users.noreply.github.com>
2023-03-26 00:41:58 -0400

    Create Gold.java

---
+++ Gold.java
@@ -0,0 +1,11 @@
+package Project;
+
+public class Gold extends State {
+    public Gold(){
+        getRank();
+    }
+
+    public void getRank(){
+        System.out.println("Rank:Gold");
+    }
+}
