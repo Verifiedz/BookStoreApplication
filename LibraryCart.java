9e65b3f018a6ef356c0d70c4160d8f64f6dd2783
reeem-a <128990860+reeem-a@users.noreply.github.com>
2023-04-01 13:22:30 -0400

    Update LibraryCart.java

--- LibraryCart.java
+++ LibraryCart.java
@@ -10,46 +10,48 @@ import java.io.FileWriter;
     private int AmountOfBooks;
     private int CustPoints;
     private int totalCartPrice;
-    ArrayList<String> CartBooks = new ArrayList<>();
+    ArrayList<Book> CartBooks = new ArrayList<>();
     
     public void BookElements(){
         //  ONLY selected book elements from files reads to arraylist 
         //for(Cartbook)
             // if book.(gettermethod for selection javafx).(checking if checkbox is selected)
-        String fileName = "bookFile.txt";
         
         try {
-            FileReader fileReader = new FileReader(fileName);
-            BufferedReader bufferedReader = new BufferedReader(fileReader);
-            String book;
-            while ((book = bufferedReader.readLine()) != null) {
-                CartBooks.add(book);
+            try (FileInputStream fileIn = new FileInputStream("books.txt")) {
+                ObjectInputStream in = new ObjectInputStream(fileIn);
+                while (true) {
+                    try {
+                        Book b = (Book) in.readObject();
+                        CartBooks.add(b);
+                    } catch (EOFException e) {
+                        break;
+                    }
+                }
+                in.close();
             }
-            bufferedReader.close();
-            fileReader.close();
-        } catch (IOException e) {
-        }
-        System.out.println(CartBooks);
+        } catch (IOException | ClassNotFoundException e) {
     }
     
-    public void Checkout(Book book, Customer customer) 
+    public void Checkout(Book book, Customer cust) 
     {
-        CustPoints = customer.getPoints();
+        CustPoints = cust.getPoints();
         //for(){ // take the books from the file using while loop
             // check if the book is selected using javaFX
                 // if book.(gettermethod for selection javafx).(checking if checkbox is selected)
                         //if it is selected then get the price of the book and add to the subtotal
-                        totalCartPrice += book.getBookPrice();
-                        System.out.println("Added to Cart: " + book.getBookName());
-                        if(customer.myState.equals(Gold())){
-                            totalCartPrice -= (totalCartPrice * 0.20); // discount of 20%
-                        }
+        
+        int size = CartBooks.size();
+        for(int i=0; i<size; i++){
+            totalCartPrice += book.getBookPrice();
+            System.out.println("Added to Cart: " + book.getBookName());
+                        
             // javaFX buy button or redeem points button
             // if selected is redeem points
-                        redeemPoints(customer, totalCartPrice);
+            redeemPoints(cust, totalCartPrice);
        // }
-            
-        customer.setPoints(CustPoints += totalCartPrice * 10); // every dollar spent 10 points added to cust points
+        }
+        cust.setPoints(CustPoints += totalCartPrice * 10); 
             
     }
     
@@ -58,12 +60,11 @@ import java.io.FileWriter;
         int pointsLeft = 0;
         int size = CartBooks.size();
             for(int i=0; i < size; i++) { 
-            
                 if(redeemedPts >= totalPrice) {
                     pointsLeft = (redeemedPts - totalPrice)*100;
                     customer.setPoints(pointsLeft);
                 }
-              //  totalCartPrice -= redeemedPts;
+                //  totalCartPrice -= redeemedPts;
                 else if(redeemedPts < totalCartPrice){
                       System.out.println("You do not have enough points!");
                 }
@@ -71,36 +72,37 @@ import java.io.FileWriter;
             
     }
         
-    public ArrayList<String> AddToCart(Book book)
+    public ArrayList<Book> AddToCart(Book book) throws IOException
     {
-        //implement javaFX add book button
-        // adds to the file
-        Books.add(book);
-        //totalCartPrice += book.getBookPrice();
+        //implement javaFX add book button after selecting 
+
+        CartBooks.add(book);
+        try (FileWriter writer = new FileWriter("bookFile.txt")) {
+            for (Book line : CartBooks) {
+                writer.write(line + "\n");
+            } 
+        }
         return CartBooks;
+        
     }
     
-    public ArrayList<String> RemovefromCart(Book book)
+    public ArrayList<Book> RemovefromCart(Book book) throws IOException
     {
         // removes selected BOOKS it  the file
         //if(book is selected)
-        BookElements();
+        // if purhcase or redeem points 
         CartBooks.remove(book);  
-        //totalCartPrice -= book.getBookPrice();
-        String bookToRemove = "example";
-        CartBooks.remove(bookToRemove);
-
-      // write the updated data to the file
-        FileWriter writer = new FileWriter("bookFile.txt");
-        for (String line : CartBooks) {
-            writer.write(line + "\n");
-      }
-        writer.close();
+        try ( // write the updated data to the file
+                FileWriter writer = new FileWriter("bookFile.txt")) {
+            for (Book line : CartBooks) {
+                writer.write(line + "\n");
+            } 
+        }
         return CartBooks;
     }
     
-    public ArrayList<String> getBooksinCart()
-    {// only selected books to display
+    public ArrayList<Book> getBooksinCart()
+    {
         return CartBooks;
     }
 }
