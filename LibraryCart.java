9a23c8282fd151945996c2a678fe71aabb59a800
Saad Wasim <105033537+Verifiedz@users.noreply.github.com>
2023-04-02 15:33:56 -0400

    Update LibraryCart.java

--- LibraryCart.java
+++ LibraryCart.java
@@ -1,72 +1,45 @@
-package project;
+
+
+package Project;
 import java.util.ArrayList;
 import java.util.Scanner;
 import java.io.IOException;
-
+import javafx.scene.control.CheckBox;
 
     public class LibraryCart {
     private int AmountOfBooks;
     private int CustPoints;
     private int totalCartPrice;
-    ArrayList<Book> CartBooks = new ArrayList<>();
+    private ArrayList<Book> CartBooks; 
     
-    
-    public void Checkout(Book book, Customer cust) 
+    public void Checkout(Book book, Customer customer)
     {
-        CustPoints = cust.getPoints();
         
-            // check if the book is selected using javaFX
-                // if book.(gettermethod for selection javafx).(checking if checkbox is selected)
-                        //if it is selected then get the price of the book and add to the subtotal
-                        //AddToCart() will be called so it updates the CartBooks Arraylist
-        int size = CartBooks.size();
-        for(int i=0; i<size; i++){
-            totalCartPrice += book.getBookPrice();
-            System.out.println("Added to Cart: " + book.getBookName());
-                        
-            // javaFX buy button or redeem points button
-            // if selected is redeem points
-            redeemPoints(cust, totalCartPrice);
-       // }
-        }
-        cust.setPoints(CustPoints += totalCartPrice * 10); 
+        for(Book b: CartBooks){
             
-    }
-    
-    public void redeemPoints(Customer customer, int totalPrice){
-        int redeemedPts = CustPoints/100; // pay w points so converting cash
-        int pointsLeft = 0;
-        int size = CartBooks.size();
-            for(int i=0; i < size; i++) { 
-                if(redeemedPts >= totalPrice) {
-                    pointsLeft = (redeemedPts - totalPrice)*100;
-                    customer.setPoints(pointsLeft);
-                }
-                //  totalCartPrice -= redeemedPts;
-                else if(redeemedPts < totalCartPrice){
-                      System.out.println("You do not have enough points!");
-                }
         }
             
+        System.out.println("Added to Cart: " + book.getBookName());
+        Double bookPrice = book.getBookPrice();
+        CustPoints = customer.getPoints();
+        customer.setPoints( CustPoints+= bookPrice * 10); // every dollar spent 10 points added to cust points
+         
+         
     }
-        
+     
     public ArrayList<Book> AddToCart(Book book)
     {
-        //implement javaFX add book button after selecting 
-
+        //implement javaFX add book button
         CartBooks.add(book);
-        
+        totalCartPrice += book.getBookPrice();
         return CartBooks;
-        
     }
     
     public ArrayList<Book> RemovefromCart(Book book)
     {
-        // removes selected BOOKS it  the file
-        //if(book is selected)
-        // if purhcase or redeem points 
-        CartBooks.remove(book);  
         
+        CartBooks.remove(book);  
+        totalCartPrice -= book.getBookPrice();
         return CartBooks;
     }
     
@@ -75,4 +48,3 @@ import java.io.IOException;
         return CartBooks;
     }
 }
-
